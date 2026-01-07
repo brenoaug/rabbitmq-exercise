6a959ba4869a7a1538c64173a6f4479b49643c1d
breno augusto <breaugustocp@outlook.com>
2026-01-07 14:01:22 -0300

    criação dos endpoints get e post de mensagens

---
+++ RabbitMq.Publisher/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMq.Publisher.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RabbitMq.Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        public static List<Message> Messages = [];
+
+        [HttpGet]
+        public IEnumerable<Message> GetMessage()
+        {
+            return Messages;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Message message)
+        {
+            Messages.Add(message);
+            return Accepted(message);
+        }
+    }
+}
---
+++ RabbitMq.Publisher/Model/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace RabbitMq.Publisher.Model
+{
+    public class Message
+    {
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+        public string? Author { get; set; }
+
+    }
+}
